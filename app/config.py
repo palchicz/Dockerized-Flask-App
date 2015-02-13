2b97524c5e1449dbcf4180d4a279b544dbca9b55
Zachary Palchick <zpalchick@5amsolutions.com>
2015-02-12 23:52:45 -0800

    Get db connection info from enviorment

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
 import os
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://docker:docker@dockerhost:7702"
+    db_user = os.environ['DB_ENV_POSTGRES_USER']
+    db_password = os.environ['DB_ENV_POSTGRES_PASSWORD']
+    db_host = os.environ['DB_PORT_5432_TCP_ADDR']
+    db_port = os.environ['DB_PORT_5432_TCP_PORT']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}'.format(
+            db_user, db_password, db_host, db_port)
