064c696e3cd1a4d97156e7d21f42ab3c28b526fb
Zachary Palchick <zpalchick@5amsolutions.com>
2015-03-18 22:20:24 -0700

    Fixes container reference in test script

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 APP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/app"
 
-docker run -e "DB_URI=sqlite:///:memory:" -v $APP_ROOT:/app -w /app flask-docker-workflow nosetests
+docker run -e "DB_URI=sqlite:///:memory:" -v $APP_ROOT:/app -w /app zpalchick/dockerized-flask-app nosetests
